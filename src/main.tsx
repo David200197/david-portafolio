dd429fe7df8d99da31a3fb27d84eda23d2682f91
David200197 <david.alfonso999999@gmail.com>
2025-01-04 02:19:08 -0500

    update react

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
 import { StrictMode } from 'react'
-import ReactDOM from 'react-dom'
-
 import { ThemeProvider } from '@/providers/Theme'
 import App from './App'
 import { composeProviders } from './utils/providers'
+import { createRoot } from 'react-dom/client'
 
 import './locales/i18n'
 
 const Provider = composeProviders([StrictMode, ThemeProvider])
 
-ReactDOM.render(
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
+
+root.render(
   <Provider>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 )
