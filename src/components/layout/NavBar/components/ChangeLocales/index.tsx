2c69dd2d20ee9fef6256a3f9bba207a300c7a087
David200197 <david.alfonso999999@gmail.com>
2025-01-09 19:17:29 -0500

    fix: switch language

--- src/components/layout/NavBar/components/ChangeLocales/index.tsx
+++ src/components/layout/NavBar/components/ChangeLocales/index.tsx
@@ -1,22 +1,18 @@
 import { useTranslation } from 'react-i18n-thin'
-import { FormControl, InputLabel, MenuItem, Select } from '@mui/material'
+import { FormControl } from '@mui/material'
+import { LanguageSwitch } from './styled'
 
 type Props = { margin?: string }
 export const ChangeLocales = ({ margin }: Props) => {
-  const { language, changeLanguage, t } = useTranslation()
+  const { language, changeLanguage } = useTranslation()
+
+  const onClick = () => {
+    changeLanguage(language === 'en' ? 'es' : 'en')
+  }
+
   return (
     <FormControl style={{ margin }} sx={{ m: 1, minWidth: 120 }} size='small'>
-      <InputLabel id='demo-simple-select-label'>{t('language')}</InputLabel>
-      <Select
-        labelId='demo-simple-select-label'
-        id='demo-simple-select'
-        value={language}
-        label='Language'
-        onChange={e => changeLanguage(e.target.value)}
-      >
-        <MenuItem value={'en'}>{t('en')}</MenuItem>
-        <MenuItem value={'es'}>{t('es')}</MenuItem>
-      </Select>
+      <LanguageSwitch onClick={onClick} defaultChecked={language === 'en'} />
     </FormControl>
   )
 }
