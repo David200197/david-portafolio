891438a2c304b94e9ff4c3d25b4b921a6e81b600
David200197 <david.alfonso999999@gmail.com>
2025-08-14 17:23:32 -0400

    update: set basePath in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "dist",
+  basePath: "/david-portafolio",
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule: any) =>
