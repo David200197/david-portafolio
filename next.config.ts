5fa7166ea713d5e4002c20c8023cadc5e6ca9feb
David200197 <david.alfonso999999@gmail.com>
2025-08-15 12:10:11 -0400

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "dist",
-  basePath: isProd ? "/david-portafolio" : "",
-  assetPrefix: isProd ? "/david-portafolio/" : "",
   images: {
     unoptimized: true,
   },
