f8227d60323e57068868e45f9fcd49974fab41d5
David200197 <david.alfonso999999@gmail.com>
2025-08-15 11:30:52 -0400

    fix

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  distDir: "dist",
+  basePath: "/david-portafolio",
   images: {
     unoptimized: true,
   },
