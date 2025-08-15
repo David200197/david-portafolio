9beb0751e6f8a4abe3e8105a09f262f3af08ec15
David200197 <david.alfonso999999@gmail.com>
2025-08-15 17:22:55 -0400

    add: assetsPrefix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/david-portafolio",
+  assetPrefix: "/david-portafolio",
   images: {
     unoptimized: true,
   },
