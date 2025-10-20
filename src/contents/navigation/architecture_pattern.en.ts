1cbec9271f6c9de6c6742dd1f82fefa4822817b7
David200197 <david.alfonso999999@gmail.com>
2025-10-20 11:50:53 -0400

    add: architecture_pattern navigation

---
+++ src/contents/navigation/architecture_pattern.en.ts
@@ -0,0 +1,86 @@
+import { ItemSideMenu } from '@/modules/blogs/model/ItemSideMenu'
+
+const items: ItemSideMenu[] = [
+  { title: 'Architecture Pattern', url: '#' },
+  {
+    title: 'Monolithic Architecture',
+    url: '#',
+  },
+  {
+    title: 'Microservices Architecture',
+    url: '#',
+  },
+  {
+    title: 'MVC (Model-View-Controller)',
+    url: '#',
+  },
+  {
+    title: 'MVP (Model-View-Presenter)',
+    url: '#',
+  },
+  {
+    title: 'MVVM (Model-View-ViewModel)',
+    url: '#',
+  },
+  {
+    title: 'Layered Architecture',
+    url: '#',
+  },
+  {
+    title: 'Hexagonal Architecture (Ports and Adapters)',
+    url: '#',
+  },
+  {
+    title: 'Clean Architecture',
+    url: '#',
+  },
+  {
+    title: 'Event-Driven Architecture',
+    url: '#',
+  },
+  {
+    title: 'Service-Oriented Architecture (SOA)',
+    url: '#',
+  },
+  {
+    title: 'CQRS (Command Query Responsibility Segregation)',
+    url: '#',
+  },
+  {
+    title: 'Pipeline Architecture',
+    url: '#',
+  },
+  {
+    title: 'Plugin Architecture',
+    url: '#',
+  },
+  {
+    title: 'Onion Architecture',
+    url: '#',
+  },
+  {
+    title: 'Space-Based Architecture',
+    url: '#',
+  },
+  {
+    title: 'Serverless Architecture',
+    url: '#',
+  },
+  {
+    title: 'Microkernel Architecture',
+    url: '#',
+  },
+  {
+    title: 'Blackboard Architecture',
+    url: '#',
+  },
+  {
+    title: 'Publish-Subscribe Architecture',
+    url: '#',
+  },
+  {
+    title: 'Pipes and Filters Architecture',
+    url: '#',
+  },
+]
+export default items
---
+++ src/contents/navigation/architecture_pattern.es.ts
@@ -0,0 +1,87 @@
+import { ItemSideMenu } from '@/modules/blogs/model/ItemSideMenu'
+
+const items: ItemSideMenu[] = [
+  { title: 'Patrón de Arquitectura', url: '#' },
+  {
+    title: 'Arquitectura Monolítica',
+    url: '#',
+  },
+  {
+    title: 'Arquitectura de Microservicios',
+    url: '#',
+  },
+  {
+    title: 'MVC (Modelo-Vista-Controlador)',
+    url: '#',
+  },
+  {
+    title: 'MVP (Modelo-Vista-Presentador)',
+    url: '#',
+  },
+  {
+    title: 'MVVM (Modelo-Vista-Modelo de Vista)',
+    url: '#',
+  },
+  {
+    title: 'Arquitectura en Capas',
+    url: '#',
+  },
+  {
+    title: 'Arquitectura Hexagonal (Puertos y Adaptadores)',
+    url: '#',
+  },
+  {
+    title: 'Arquitectura Limpia',
+    url: '#',
+  },
+  {
+    title: 'Arquitectura Basada en Eventos',
+    url: '#',
+  },
+  {
+    title: 'Arquitectura Orientada a Servicios (SOA)',
+    url: '#',
+  },
+  {
+    title: 'CQRS (Segregación de Responsabilidad de Consulta y Comando)',
+    url: '#',
+  },
+  {
+    title: 'Arquitectura de Pipeline',
+    url: '#',
+  },
+  {
+    title: 'Arquitectura de Plugin',
+    url: '#',
+  },
+  {
+    title: 'Arquitectura de Cebolla',
+    url: '#',
+  },
+  {
+    title: 'Arquitectura Basada en Espacio',
+    url: '#',
+  },
+  {
+    title: 'Arquitectura Sin Servidor',
+    url: '#',
+  },
+  {
+    title: 'Arquitectura de Micronúcleo',
+    url: '#',
+  },
+  {
+    title: 'Arquitectura de Pizarra',
+    url: '#',
+  },
+  {
+    title: 'Arquitectura Publicar-Suscribir',
+    url: '#',
+  },
+  {
+    title: 'Arquitectura de Tuberías y Filtros',
+    url: '#',
+  },
+]
+
+export default items
